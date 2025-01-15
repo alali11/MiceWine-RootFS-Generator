57b44246e8215afa774fb96be8d74a64b0ef593b
Anonymus04 <joaobat0102@gmail.com>
2025-01-15 20:00:42 -0300

    Update build.sh

--- packages/mesa-panfork/build.sh
+++ packages/mesa-panfork/build.sh
@@ -5,4 +5,4 @@ PKG_PRETTY_NAME="Mesa Panfrost Driver"
 GIT_URL=https://github.com/anonymususer04/mesa-Panfork-android
 GIT_COMMIT=0a9c14e
 LDFLAGS="-L$PREFIX/lib -landroid-shmem -lxcb-xfixes"
-MESON_ARGS="-Dgallium-drivers=panfrost,swrast -Dvulkan-drivers= -Dglvnd=enabled -Dplatforms=x11 -Dxmlconfig=disabled -Dllvm=disabled -Dopengl=true -Degl=disabled -Dgbm=true -Dzstd=enabled" 
+MESON_ARGS="-Dgallium-drivers=panfrost,swrast -Dvulkan-drivers= -Dglvnd=true -Dplatforms=x11 -Dxmlconfig=disabled -Dllvm=disabled -Dopengl=true -Degl=enabled -Dgbm=true -Dzstd=enabled" 
