d93bc87a8a1501124c24d952bb936dc4b8ab0a3a
alali11 <joaobatista07032@gmail.com>
2025-01-30 18:04:23 -0300

    Update build.sh

--- packages/mesa-zink/build.sh
+++ packages/mesa-zink/build.sh
@@ -1,8 +1,8 @@
 PKG_VER=[gss]
 PKG_CATEGORY="OpenGLDriver"
-PKG_PRETTY_NAME="Mesa Zink Driver"
+PKG_PRETTY_NAME="Mesa Zink (xlib) Driver"
 
-GIT_URL=https://gitlab.freedesktop.org/mesa/mesa
-GIT_COMMIT=ed64ecc
+GIT_URL=https://github.com/alexvorxx/zink-xlib-termux
+GIT_COMMIT=c1567e9
 LDFLAGS="-L$PREFIX/lib -landroid-shmem -lxcb-xfixes"
-MESON_ARGS="-Dgallium-drivers=zink -Dvulkan-drivers= -Dglvnd=enabled -Dplatforms=x11 -Dxmlconfig=disabled -Dllvm=disabled -Dopengl=true -Degl=disabled -Dzstd=enabled" 
+MESON_ARGS="-Dgallium-drivers=zink,swrast -Dvulkan-drivers= -Dglvnd=enabled -Dplatforms=x11 -Dxmlconfig=disabled -Dllvm=disabled -Dgallium-va=disabled -Dopengl=true -Degl=enabled Ddri3=disabled -Dglx=xlib -Dzstd=enabled" 
